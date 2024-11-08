8cc9d5c9eb428a061530aa37bed1fd2b56290c00
Sohail <ahnrd43@gmail.com>
2024-11-08 10:14:47 +0300

    Allow cookies to be sent from React app

--- API/Program.cs
+++ API/Program.cs
@@ -43,7 +43,7 @@ catch (Exception ex)
 
 
 app.UseCors((options => {
-    options.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+    options.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000");
 }));
 
 app.Run();

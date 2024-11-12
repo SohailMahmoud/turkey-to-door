fc4e82fe17693ba580dd6a0c458684266fb778c1
Sohail <ahnrd43@gmail.com>
2024-11-12 09:27:39 +0300

    Feature: add cart logic

--- API/Data/DbInitializer.cs
+++ API/Data/DbInitializer.cs
@@ -1,4 +1,5 @@
 using API.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
@@ -6,206 +7,72 @@ namespace API.Data
     {
         public static void Initialize(StoreContext context)
         {
-            if(context.Products.Any()) return;
+            // if(context.Products.Any()) return;
+
+            // Remove all existing products
+            if (context.Products.Any())
+            {
+                context.Products.RemoveRange(context.Products);
+                Console.WriteLine("Data cleared");
+            }
+
+            // Reset the identity column (auto-increment) for SQLite
+            context.Database.ExecuteSqlRaw("DELETE FROM sqlite_sequence WHERE name='Products'");
 
             var products = new List<Product> 
             {
                 new Product
                 {
-                    Name = "Angular Speedster Board 2000",
-                    Description =
-                        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
-                    Price = 20000,
-                    PictureUrl = "/images/products/sb-ang1.png",
-                    Brand = "Angular",
-                    Type = "Boards",
+                    Name = "Kuru Baklava",
+                    Description ="كورو بقلاوة",
+                    Price = 70000,
+                    PictureUrl = "/fistikli-kuru-baklava.jpg",
+                    Brand = "Turkey To Door",
+                    Type = "Baklava",
                     QuantityInStock = 100
                 },
                 new Product
                 {
-                    Name = "Green Angular Board 3000",
-                    Description = "Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus.",
-                    Price = 15000,
-                    PictureUrl = "/images/products/sb-ang2.png",
-                    Brand = "Angular",
-                    Type = "Boards",
+                    Name = "Sarı Burma",
+                    Description = "بقلاوة بورما بعين الجمل",
+                    Price = 75000,
+                    PictureUrl = "/sarma-burma.jpg",
+                    Brand = "Turkey To Door",
+                    Type = "Baklava",
                     QuantityInStock = 100
                 },
                 new Product
                 {
-                    Name = "Core Board Speed Rush 3",
-                    Description =
-                        "Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
-                    Price = 18000,
-                    PictureUrl = "/images/products/sb-core1.png",
-                    Brand = "NetCore",
-                    Type = "Boards",
+                    Name = "Hasır Kadayıf",
+                    Description ="كنافة مربعات بالفسدق الكامل",
+                    Price = 80000,
+                    PictureUrl = "/hasir-kadayif.jpg",
+                    Brand = "Turkey To Door",
+                    Type = "Kunefe",
                     QuantityInStock = 100
                 },
                 new Product
                 {
-                    Name = "Net Core Super Board",
+                    Name = "Cevizli Baklava",
                     Description =
-                        "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.",
-                    Price = 30000,
-                    PictureUrl = "/images/products/sb-core2.png",
-                    Brand = "NetCore",
-                    Type = "Boards",
+                        "بقلاوة طبقات مقرمشة متحمرة و محشية عين جمل تركي مميز",
+                    Price = 65000,
+                    PictureUrl = "/cevizli-baklava.jpg",
+                    Brand = "Turkey To Door",
+                    Type = "Baklava",
                     QuantityInStock = 100
                 },
                 new Product
                 {
-                    Name = "React Board Super Whizzy Fast",
+                    Name = "Karadut Fıstıklı Halep Türk Lokumu",
                     Description =
-                        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                        "حلقوم اصابع بالتوت و الفسدق",
                     Price = 25000,
                     PictureUrl = "/images/products/sb-react1.png",
-                    Brand = "React",
-                    Type = "Boards",
+                    Brand = "Turkey To Door",
+                    Type = "Lokum",
                     QuantityInStock = 100
                 },
-                new Product
-                {
-                    Name = "Typescript Entry Board",
-                    Description =
-                        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
-                    Price = 12000,
-                    PictureUrl = "/images/products/sb-ts1.png",
-                    Brand = "TypeScript",
-                    Type = "Boards",
-                    QuantityInStock = 100
-                },
-                new Product
-                {
-                    Name = "Core Blue Hat",
-                    Description =
-                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
-                    Price = 1000,
-                    PictureUrl = "/images/products/hat-core1.png",
-                    Brand = "NetCore",
-                    Type = "Hats",
-                    QuantityInStock = 100
-                },
-                new Product
-                {
-                    Name = "Green React Woolen Hat",
-                    Description =
-                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
-                    Price = 8000,
-                    PictureUrl = "/images/products/hat-react1.png",
-                    Brand = "React",
-                    Type = "Hats",
-                    QuantityInStock = 100
-                },
-                new Product
-                {
-                    Name = "Purple React Woolen Hat",
-                    Description =
-                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
-                    Price = 1500,
-                    PictureUrl = "/images/products/hat-react2.png",
-                    Brand = "React",
-                    Type = "Hats",
-                    QuantityInStock = 100
-                },
-                new Product
-                {
-                    Name = "Blue Code Gloves",
-                    Description =
-                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
-                    Price = 1800,
-                    PictureUrl = "/images/products/glove-code1.png",
-                    Brand = "VS Code",
-                    Type = "Gloves",
-                    QuantityInStock = 100
-                },
-                new Product
-                {
-                    Name = "Green Code Gloves",
-                    Description =
-                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
-                    Price = 1500,
-                    PictureUrl = "/images/products/glove-code2.png",
-                    Brand = "VS Code",
-                    Type = "Gloves",
-                    QuantityInStock = 100
-                },
-                new Product
-                {
-                    Name = "Purple React Gloves",
-                    Description =
-                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
-                    Price = 1600,
-                    PictureUrl = "/images/products/glove-react1.png",
-                    Brand = "React",
-                    Type = "Gloves",
-                    QuantityInStock = 100
-                },
-                new Product
-                {
-                    Name = "Green React Gloves",
-                    Description =
-                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
-                    Price = 1400,
-                    PictureUrl = "/images/products/glove-react2.png",
-                    Brand = "React",
-                    Type = "Gloves",
-                    QuantityInStock = 100
-                },
-                new Product
-                {
-                    Name = "Redis Red Boots",
-                    Description =
-                        "Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
-                    Price = 25000,
-                    PictureUrl = "/images/products/boot-redis1.png",
-                    Brand = "Redis",
-                    Type = "Boots",
-                    QuantityInStock = 100
-                },
-                new Product
-                {
-                    Name = "Core Red Boots",
-                    Description =
-                        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
-                    Price = 18999,
-                    PictureUrl = "/images/products/boot-core2.png",
-                    Brand = "NetCore",
-                    Type = "Boots",
-                    QuantityInStock = 100
-                },
-                new Product
-                {
-                    Name = "Core Purple Boots",
-                    Description =
-                        "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.",
-                    Price = 19999,
-                    PictureUrl = "/images/products/boot-core1.png",
-                    Brand = "NetCore",
-                    Type = "Boots",
-                    QuantityInStock = 100
-                },
-                new Product
-                {
-                    Name = "Angular Purple Boots",
-                    Description = "Aenean nec lorem. In porttitor. Donec laoreet nonummy augue.",
-                    Price = 15000,
-                    PictureUrl = "/images/products/boot-ang2.png",
-                    Brand = "Angular",
-                    Type = "Boots",
-                    QuantityInStock = 100
-                },
-                new Product
-                {
-                    Name = "Angular Blue Boots",
-                    Description =
-                        "Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
-                    Price = 18000,
-                    PictureUrl = "/images/products/boot-ang1.png",
-                    Brand = "Angular",
-                    Type = "Boots",
-                    QuantityInStock = 100
-                }
             };
             
             foreach (var product in products)
@@ -214,6 +81,7 @@ namespace API.Data
             }
 
             context.SaveChanges();
+            
         }
     }
 }
--- client/src/Components/ProductsList/ProductsList.tsx
+++ client/src/Components/ProductsList/ProductsList.tsx
@@ -1,45 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import agent from "../../app/agent";
+import { Product } from '../../models/product';
 
-const products = [
-    {
-        Id: 1,
-        Name: 'Dolma Baklava',
-        href: '#',
-        PictureUrl: '/dolma-baklava.jpg',
-        PictureAlt: "Front of men's Basic Tee in black.",
-        Price: '$35',
-    },
-    {
-        Id: 2,
-        Name: 'Lokum',
-        href: '#',
-        PictureUrl: '/lokum.jpg',
-        PictureAlt: "Front of men's Basic Tee in black.",
-        Price: '$35',
-    },
-    {
-        Id: 3,
-        Name: 'Turkish Coffee',
-        href: '#',
-        PictureUrl: '/kahve.jpg',
-        PictureAlt: "Front of men's Basic Tee in black.",
-        Price: '$35',
-    },
-    {
-        Id: 4,
-        Name: 'Kunefe',
-        href: '#',
-        PictureUrl: '/kunefe.jpg',
-        PictureAlt: "Front of men's Basic Tee in black.",
-        Price: '$35',
-    },
-    // More products...
-];
+
 
 export default function ProductList() {
     // Track loading state per product (use an object to store loading state by product Id)
     const [loadingStates, setLoadingStates] = useState<{ [key: number]: boolean }>({});
+
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        agent.Catalog.list()
+            .then((list) => {
+                console.log(list)
+                setProducts(list)
+            })
+            .catch((err) => console.log(err));
+    }, [])
 
     async function handleAddToBag(productId: number) {
         // Set the specific product as loading
@@ -65,32 +43,33 @@ export default function ProductList() {
                 </div>
 
                 <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                    {products.map((product) => (
-                        <div key={product.Id} className="group relative">
+                    {products.slice(0, 4).map((product) => (
+                        <div key={product.id} className="group relative">
                             <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                                 <img
-                                    alt={product.PictureAlt}
-                                    src={product.PictureUrl}
+                                    // alt={product.PictureAlt}
+                                    src={product.pictureUrl}
                                     className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                                 />
                             </div>
                             <div className="mt-4 flex justify-between">
                                 <div>
                                     <h3 className="text-sm text-gray-700">
-                                        <a href={product.href}>
-                                            {product.Name}
+                                        <a href="#">
+                                            {product.name}
                                         </a>
                                     </h3>
+                                    <p className="mt-1 text-sm text-gray-500">1 KG</p>
                                 </div>
-                                <p className="text-sm font-medium text-gray-900">{product.Price}</p>
+                                <p className="text-sm font-medium text-gray-900">IQD {product.price.toLocaleString()}</p>
                             </div>
                             <div className="mt-6 text-center">
                                 <button
-                                    onClick={() => handleAddToBag(product.Id)}
+                                    onClick={() => handleAddToBag(product.id)}
                                     className="flex justify-center bg-gray-100 rounded-md text-center px-8 py-2 w-full"
-                                    disabled={loadingStates[product.Id]}
+                                    disabled={loadingStates[product.id]}
                                 >
-                                    {loadingStates[product.Id] ? (
+                                    {loadingStates[product.id] ? (
                                         <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                                             <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                                             <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
